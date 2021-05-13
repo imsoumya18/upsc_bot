c930c0fc4e3013b050d87a2aeda6585ce98c2f3f
imsoumya18 <soumyadeep184@gmail.com>
2021-05-13 23:06:54 +0530

    init

--- main.py
+++ main.py
@@ -1,34 +1,24 @@
-from selenium import webdriver
-from datetime import date
-import calendar
-import time
-from idm import IDMan
+import discord
 import requests
+from bs4 import BeautifulSoup
+import schedule
 
-downloader = IDMan()
+client = discord.Client()
 
-date_serch = str(date.today().day) + ' ' + calendar.month_name[date.today().month] + ' ' + str(date.today().year)
-print(date_serch)
 
-profile = webdriver.FirefoxProfile()
-profile.set_preference("browser.download.folderList", 2)
-profile.set_preference("browser.download.manager.showWhenStarting", False)
-profile.set_preference("browser.download.dir", '/downloads')
-profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/x-gzip")
-driver = webdriver.Firefox(firefox_profile=profile)
+def job():
+    res = requests.get('https://www.visioniascurrentaffairs.com')
+    soup = BeautifulSoup(res.text, 'html.parser')
+    url = soup.find("a", attrs={"class": "green"})
+    channel = client.get_channel(842452759705944124)
+    channel.send(url.get('href'))
 
-driver.get('https://dailyepaper.in/the-hindu-pdf-epaper-downloa-now/')
-#
-# time.sleep(5)
-#
-# driver.find_element_by_id('webpushr-deny-button').click()
-url = driver.find_element_by_xpath("//p[contains(text(), '" + "11 May 2021" + "')]/a").get_attribute('href')
-# downloader.download(url, "downloads", output=None, referrer=None, cookie=None, postData=None, user=None, password=None, confirm = False, lflag = None, clip=False)
-# time.sleep(60)
-# driver.find_element_by_xpath("//button[contains(text(), 'Download file')]").click()
-response = requests.get(url)
-with open('/data.pdf') as f:
-    f.write(response.content)
 
-driver.quit()
+@client.event
+async def on_ready():
+    print('Started')
 
+
+client.run('ODQyMzc2MDkyNTA1NDczMDc0.YJ0Z3w.7zWHI6AnHTdBz0egq0kEujsMtV4')
+
+schedule.every().day.at('23:04')
---
+++ main2.py
@@ -0,0 +1,3 @@
+import os.path
+
+print(os.path.isfile('newspaper.pdf'))
