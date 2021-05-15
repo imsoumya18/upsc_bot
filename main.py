cb2e1abc723498d6582e8912107f6d54b61866ef
imsoumya18 <soumyadeep184@gmail.com>
2021-05-15 10:45:04 +0530

    commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import discord
+import requests
+from bs4 import BeautifulSoup
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('Started')
+
+
+@client.event
+async def on_message(message):
+    if message.content == 'newspaper':
+        res = requests.get('https://www.visioniascurrentaffairs.com')
+        soup = BeautifulSoup(res.text, 'html.parser')
+        url = soup.find("a", attrs={"class": "green"})
+        await message.channel.send('Today\'s The Hindu:\n' + url.get('href'))
+
+
+client.run('ODQyMzc2MDkyNTA1NDczMDc0.YJ0Z3w.7zWHI6AnHTdBz0egq0kEujsMtV4')
