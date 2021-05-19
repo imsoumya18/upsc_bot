38e03bb0ebf0001811fcf0697965a5fca7977859
imsoumya18 <soumyadeep184@gmail.com>
2021-05-19 08:40:37 +0530

    'yojana'

--- main.py
+++ main.py
@@ -15,12 +15,19 @@ async def on_message(message):
     if message.content.lower() == '--help':
         embedparam = discord.Embed(title='--help', description='Get help', color=0x0addd7)
         embedparam.add_field(name="--hindu", value="Get daily The Hindu newspaper PDF", inline=False)
+        embedparam.add_field(name="--yojana", value="Get monthly Yojana Magazine PDF", inline=False)
         await message.channel.send(embed=embedparam)
     elif message.content.lower() == '--hindu':
         res = requests.get('https://www.visioniascurrentaffairs.com')
         soup = BeautifulSoup(res.text, 'html.parser')
         url = soup.find("a", attrs={"class": "green"})
         embedparam = discord.Embed(title=url.getText(), description=url.get('href'), color=0x0addd7)
+        await message.channel.send(embed=embedparam)
+    elif message.content.lower() == '--yojana':
+        res = requests.get('https://chahalacademy.com/free-downloads-yojana')
+        soup = BeautifulSoup(res.text, 'html.parser')
+        url = soup.find("a", string='Open File')
+        embedparam = discord.Embed(title=url.get('title'), description='https://chahalacademy.com/' + url.get('href'), color=0x0addd7)
         await message.channel.send(embed=embedparam)
 
 
