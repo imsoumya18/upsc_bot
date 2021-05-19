fc93bc74e45bc895bea8cf2b92e24bd229763f95
imsoumya18 <soumyadeep184@gmail.com>
2021-05-19 20:02:53 +0530

    hyperlink

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import discord
 import requests
 from bs4 import BeautifulSoup
 
+TOKEN = 'TOKEN(str)'
+OWN_ID = 'YOUR ID(int)'
+
 bot = discord.Client()
 
 
@@ -26,15 +29,18 @@ async def on_message(message):
         res = requests.get('https://www.visioniascurrentaffairs.com')
         soup = BeautifulSoup(res.text, 'html.parser')
         url = soup.find("a", attrs={"class": "green"})
-        embedparam = discord.Embed(title=url.getText(), description='[Download]({})'.format(url.get('href')), color=0x0addd7)
+        embedparam = discord.Embed(title=url.getText(), description='[Download]({})'.format(url.get('href')),
+                                   color=0x0addd7)
         await message.channel.send(embed=embedparam)
     elif message.content.lower() == '--yojana':
         res = requests.get('https://chahalacademy.com/free-downloads-yojana')
         soup = BeautifulSoup(res.text, 'html.parser')
         url = soup.find("a", string='Open File')
-        embedparam = discord.Embed(title=url.get('title'), description='[Download]({})'.format('https://chahalacademy.com/' + url.get('href')), color=0x0addd7)
+        embedparam = discord.Embed(title=url.get('title'),
+                                   description='[Download]({})'.format('https://chahalacademy.com/' + url.get('href')),
+                                   color=0x0addd7)
         await message.channel.send(embed=embedparam)
-    elif message.content.lower() == '--server' and message.author.id == 832576008149794818:
+    elif message.content.lower() == '--server' and message.author.id == OWN_ID:
         servers = []
         async for guild in bot.fetch_guilds(limit=150):
             servers.append(guild.name)
@@ -42,4 +48,4 @@ async def on_message(message):
         await message.channel.send(embed=embedparam)
 
 
-bot.run('TOKEN')
+bot.run(TOKEN)
