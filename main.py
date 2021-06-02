be47a97fd9ee7a8f743d852779e465585d279acb
imsoumya18 <soumyadeep184@gmail.com>
2021-06-02 09:03:20 +0530

    chng

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import discord
+from datetime import datetime, time, timedelta
 import requests
 from bs4 import BeautifulSoup
+import asyncio
 
 TOKEN = 'TOKEN(str)'               # Replace with your token
 OWN_ID = 'YOUR ID(int)'            # Replace with your own id
+WHEN = time(3, 0, 0)
 
 bot = discord.Client()
 
@@ -11,6 +14,32 @@ bot = discord.Client()
 @bot.event
 async def on_ready():
     print('Started')
+    # while True:
+    #     res = requests.get('https://www.visioniascurrentaffairs.com')
+    #     soup = BeautifulSoup(res.text, 'html.parser')
+    #     url = soup.find('a', attrs={'class': 'green'})
+    #     embedparam = discord.Embed(title=url.getText(), description='[Download]({})'.format(url.get('href')),
+    #                                color=0x0addd7)
+    #     await bot.get_channel(845113175561076827).send(embed=embedparam)
+    #     await asyncio.sleep(1)
+
+
+async def called_once_a_day():
+    await bot.wait_until_ready()
+    channel = bot.get_channel(845113175561076827)
+    await channel.send('Loop running')
+
+
+async def background_task():
+    while True:
+        now = datetime.utcnow()
+        target_time = datetime.combine(now.date(), WHEN)
+        seconds_until_target = (target_time - now).total_seconds()
+        await asyncio.sleep(seconds_until_target)
+        await called_once_a_day()
+        tomorrow = datetime.combine(now.date() + timedelta(days=1), time(0))
+        seconds = (tomorrow - now).total_seconds()
+        await asyncio.sleep(seconds)
 
 
 @bot.event
@@ -19,8 +48,9 @@ async def on_message(message):
         embedparam = discord.Embed(title='--help', description='Get help', color=0x0addd7)
         embedparam.add_field(name='--hindu', value='Get daily The Hindu newspaper PDF', inline=False)
         embedparam.add_field(name='--yojana', value='Get monthly Yojana magazine PDF', inline=False)
-        if message.author.id == OWN_ID:
-            embedparam.add_field(name='---------------Extras---------------', value='Extra commands for DEVELOPER ONLY', inline=False)
+        if message.author.id == OWN_ID and message.channel.id == 847856568581357578:
+            embedparam.add_field(name='---------------Extras---------------', value='Extra commands for DEVELOPER ONLY',
+                                 inline=False)
             embedparam.add_field(name='--server', value='Get all servers list', inline=False)
             embedparam.add_field(name='--channel', value='Get all channels list', inline=False)
         await message.channel.send(embed=embedparam)
@@ -42,18 +72,21 @@ async def on_message(message):
                                    color=0x0addd7)
         await message.channel.send(embed=embedparam)
 
-    elif message.content.lower() == '--server' and message.author.id == OWN_ID:
+    elif message.content.lower() == '--server' and message.author.id == OWN_ID and message.channel.id == 847856568581357578:
         servers = []
         async for guild in bot.fetch_guilds(limit=150):
             servers.append(guild.name)
         embedparam = discord.Embed(title='Server List', description='\n'.join(servers), color=0x0addd7)
         await message.channel.send(embed=embedparam)
 
-    elif message.content.lower() == '--channel' and message.author.id == OWN_ID:
+    elif message.content.lower() == '--channel' and message.author.id == OWN_ID and message.channel.id == 847856568581357578:
         for guild in bot.guilds:
             text_channel_list = []
             for channel in guild.text_channels:
-                text_channel_list.append(channel.name)
+                try:
+                    text_channel_list.append(channel.name)
+                except:
+                    text_channel_list.append('<Censored>')
             if guild == bot.guilds[0]:
                 embedparam = discord.Embed(title=guild.name, description='\n'.join(text_channel_list), color=0x0addd7)
             else:
@@ -61,4 +94,5 @@ async def on_message(message):
         await message.channel.send(embed=embedparam)
 
 
+bot.loop.create_task(background_task())
 bot.run(TOKEN)
