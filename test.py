de549243f42890e3cd693ba2d5b8925a2492d9c2
imsoumya18 <soumyadeep184@gmail.com>
2021-06-05 14:06:41 +0530

    chng

--- test.py
+++ test.py
@@ -3,3 +3,5 @@ from bs4 import BeautifulSoup
 
 res = requests.get('https://iasbano.com/indian-express-upsc.php#download_the_hindu', headers={"User-Agent": "XY"})
 print(res)
+# res = requests.get('https://newspaperpdf.online/download-indian-express.php', headers={"User-Agent": "XY"})
+# print(res)
