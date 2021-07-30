73a88c5b30f1c86cd45b99ffc554fccb496c509f
imsoumya18 <soumyadeep184@gmail.com>
2021-07-30 14:44:23 +0530

    send message and time change

--- test.py
+++
@@ -1,8 +0,0 @@
-x = '--send [123, 125, 127] hi how are you'
-msg = x[x.find(']')+2:]
-x = x[x.find('[')+1: x.find(']')].split(', ')
-xint = []
-for i in x:
-    xint.append(int(i))
-print(xint)
-print(msg)
