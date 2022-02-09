91b885bcee47839638960584de3ec42327a410a7
imsoumya18 <soumyadeep184@gmail.com>
2022-02-09 14:48:52 +0530

    insights ias added

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ COUNTDOWN_CHANNEL = 'COUNTDOWN_CHANNEL_ID(int)'  # Countdown Channel ID
 THE_HINDU_CHANNELS = ['LIST OF THE HINDU CHANNEL IDS(int)']  # The Hindu Channel IDs
 VISION_IAS_CHANNELS = ['LIST OF VISION IAS CHANNEL IDS(int)']  # Vision IAS Channel IDs
 NEXT_IAS_CHANNELS = ['LIST OF NEXT IAS CHANNEL IDS(int)']  # Next IAS Channel IDs
-ANS_WRITING_RECORD_CHANNEL = 'ANSWER WRITING RECORD CHANNEL(int)'  # Answer Writing Record Channel
+INSIGHTS_IAS_CHANNELS = ['LIST OF INSIGHTS IAS CHANNEL IDS(int)']  # Insights IAS Channel IDs
 WHEN = (datetime.combine(date.today(), time(7, 00, 00)) + timedelta(hours=-5, minutes=-30)).time()  # IST Time
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/spreadsheets',
          'https://www.googleapis.com/auth/drive.file', 'https://www.googleapis.com/auth/drive']
@@ -80,6 +80,20 @@ def next_mcq():
     element = soup.find("div", {"class": "table_inner_content_text"})
     title = 'Next IAS MCQ ' + element.find('h6').getText()
     url = element.find('a').get('href')
+    return [title, url]
+
+
+def insights_ca():
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"}
+    res = requests.get('https://www.insightsonindia.com/tag/current-affairs-monthly-compilations', headers=headers)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    ele = soup.find('article').find('h2').find('a')
+    res = requests.get(ele.get('href'), headers=headers)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    ele = soup.find_all('h2')[1].find('a')
+    title = ele.getText()
+    url = ele.get('href')
     return [title, url]
 
 
@@ -152,6 +166,27 @@ async def called_once_a_day():
         secret_sheet.delete_rows(2)
         secret_sheet.insert_row([vals[0]], 2)
 
+    # insights
+    vals = insights_ca()
+    if vals[0] != secret_sheet.cell(3, 1).value:
+        embedparam = discord.Embed(title=vals[0], description='[Download]({})'.format(vals[1]), color=0x0addd7)
+        sent = []
+        failed = []
+        for i in INSIGHTS_IAS_CHANNELS:
+            try:
+                await bot.get_channel(i).send(embed=embedparam)
+                sent.append(str(i))
+            except:
+                failed.append(str(i))
+                continue
+        embedparam2 = discord.Embed(title='Insights IAS sent to :white_check_mark:', description=' '.join(sent),
+                                    color=0x0addd7)
+        if len(failed) != 0:
+            embedparam2.add_field(name='Failed :no_entry_sign:', value=' '.join(failed))
+        await bot.get_channel(DEVELOPER_PRIVATE_CHANNEL).send(embed=embedparam2)
+        secret_sheet.delete_rows(3)
+        secret_sheet.insert_row([vals[0]], 3)
+
     # countdown
     prelims = datetime(2023, 6, 1)
     today = datetime.today()
@@ -182,10 +217,13 @@ async def on_message(message):
         embedparam.add_field(name='--hindu', value='Get latest The Hindu newspaper PDF', inline=False)
         embedparam.add_field(name='--vision', value='Get latest Vision IAS Current Affairs PDF', inline=False)
         embedparam.add_field(name='--next', value='Get latest Next IAS Monthly MCQ PDF', inline=False)
+        embedparam.add_field(name='--insights', value='Get latest Insight IAS Current Affairs PDF', inline=False)
         embedparam.add_field(name='--add_hindu', value='Add the channel to get daily The Hindu', inline=False)
         embedparam.add_field(name='--add_vision', value='Add the channel to get monthly Vision IAS Magazine',
                              inline=False)
         embedparam.add_field(name='--add_next', value='Add the channel to get monthly Next IAS MCQ PDF', inline=False)
+        embedparam.add_field(name='--add_insights', value='Add the channel to get monthly Insights IAS Magazine',
+                             inline=False)
         if message.author.id == DEVELOPER_ID and message.channel.id == DEVELOPER_PRIVATE_CHANNEL:
             embedparam.add_field(name='---------------Extras---------------', value='Extra commands for DEVELOPER ONLY',
                                  inline=False)
@@ -198,6 +236,9 @@ async def on_message(message):
                                  inline=False)
             embedparam.add_field(name='--send_next <Channel ID(s)>',
                                  value='Send Next IAS MCQ to channels immediately',
+                                 inline=False)
+            embedparam.add_field(name='--send_insights <Channel ID(s)>',
+                                 value='Send Insights IAS magazine to channels immediately',
                                  inline=False)
             embedparam.add_field(name='--resend',
                                  value='Resend all to all channels immediately',
@@ -228,7 +269,7 @@ async def on_message(message):
                                     color=0x0addd7)
         embedparam2.add_field(name='Requested By',
                               value=str(message.author.name) + '#' + str(message.author.discriminator))
-        embedparam2.add_field(name='Server name', value=str(message.author.guild.name))
+        embedparam2.add_field(name='Server Name', value=str(message.author.guild.name))
         await message.channel.send(embed=embedparam)
         await bot.get_channel(DEVELOPER_PRIVATE_CHANNEL).send(embed=embedparam2)
         await message.delete()
@@ -241,7 +282,21 @@ async def on_message(message):
                                     color=0x0addd7)
         embedparam2.add_field(name='Requested By',
                               value=str(message.author.name) + '#' + str(message.author.discriminator))
-        embedparam2.add_field(name='Server name', value=str(message.author.guild.name))
+        embedparam2.add_field(name='Server Name', value=str(message.author.guild.name))
+        await message.channel.send(embed=embedparam)
+        await bot.get_channel(DEVELOPER_PRIVATE_CHANNEL).send(embed=embedparam2)
+        await message.delete()
+
+    elif message.content.lower() == '--insights':
+        # insights
+        vals = insights_ca()
+        embedparam = discord.Embed(title=vals[0], description='[Download]({})'.format(vals[1]), color=0x0addd7)
+        embedparam2 = discord.Embed(title='Insights IAS Request :white_check_mark:',
+                                    description=str(message.channel.id),
+                                    color=0x0addd7)
+        embedparam2.add_field(name='Requested By',
+                              value=str(message.author.name) + '#' + str(message.author.discriminator))
+        embedparam2.add_field(name='Server Name', value=str(message.author.guild.name))
         await message.channel.send(embed=embedparam)
         await bot.get_channel(DEVELOPER_PRIVATE_CHANNEL).send(embed=embedparam2)
         await message.delete()
@@ -276,6 +331,18 @@ async def on_message(message):
         await bot.get_channel(REQUEST_CHANNEL).send(embed=embedparam)
         embedparam = discord.Embed(title='Channel Added',
                                    description='Next IAS MCQ will be sent monthly in this channel as soon as developer approves',
+                                   color=0x0addd7)
+        await message.channel.send(embed=embedparam)
+
+    elif message.content.lower() == '--add_insights':
+        embedparam = discord.Embed(title='Insights IAS Add Request', description=str(message.channel.id),
+                                   color=0x0addd7)
+        embedparam.add_field(name='Requested By',
+                             value=str(message.author.name) + '#' + str(message.author.discriminator))
+        embedparam.add_field(name='Server name', value=str(message.author.guild.name))
+        await bot.get_channel(REQUEST_CHANNEL).send(embed=embedparam)
+        embedparam = discord.Embed(title='Channel Added',
+                                   description='Insights IAS MCQ will be sent monthly in this channel as soon as developer approves',
                                    color=0x0addd7)
         await message.channel.send(embed=embedparam)
 
@@ -352,7 +419,28 @@ async def on_message(message):
             embedparam2.add_field(name='Failed :no_entry_sign:', value=' '.join(failed))
         await bot.get_channel(DEVELOPER_PRIVATE_CHANNEL).send(embed=embedparam2)
 
-    elif message.content.startswith('--resend') and message.author.id == DEVELOPER_ID and message.channel.id == DEVELOPER_SEND_CHANNEL:
+    elif message.content.startswith(
+            '--send_insights') and message.author.id == DEVELOPER_ID and message.channel.id == DEVELOPER_SEND_CHANNEL:
+        # insights
+        vals = insights_ca()
+        embedparam = discord.Embed(title=vals[0], description='[Download]({})'.format(vals[1]), color=0x0addd7)
+        sent = []
+        failed = []
+        for i in message.content.split()[1:]:
+            try:
+                await bot.get_channel(int(i)).send(embed=embedparam)
+                sent.append(i)
+            except:
+                failed.append(i)
+                continue
+        embedparam2 = discord.Embed(title='Insights IAS sent to :white_check_mark:', description=' '.join(sent),
+                                    color=0x0addd7)
+        if len(failed) != 0:
+            embedparam2.add_field(name='Failed :no_entry_sign:', value=' '.join(failed))
+        await bot.get_channel(DEVELOPER_PRIVATE_CHANNEL).send(embed=embedparam2)
+
+    elif message.content.startswith(
+            '--resend') and message.author.id == DEVELOPER_ID and message.channel.id == DEVELOPER_SEND_CHANNEL:
         # resend all
         # hindu
         vals = hindu()
@@ -414,6 +502,27 @@ async def on_message(message):
             await bot.get_channel(DEVELOPER_PRIVATE_CHANNEL).send(embed=embedparam2)
             secret_sheet.delete_rows(2)
             secret_sheet.insert_row([vals[0]], 2)
+
+        # insights
+        vals = insights_ca()
+        if vals[0] != secret_sheet.cell(3, 1).value:
+            embedparam = discord.Embed(title=vals[0], description='[Download]({})'.format(vals[1]), color=0x0addd7)
+            sent = []
+            failed = []
+            for i in INSIGHTS_IAS_CHANNELS:
+                try:
+                    await bot.get_channel(i).send(embed=embedparam)
+                    sent.append(str(i))
+                except:
+                    failed.append(str(i))
+                    continue
+            embedparam2 = discord.Embed(title='Insights IAS sent to :white_check_mark:', description=' '.join(sent),
+                                        color=0x0addd7)
+            if len(failed) != 0:
+                embedparam2.add_field(name='Failed :no_entry_sign:', value=' '.join(failed))
+            await bot.get_channel(DEVELOPER_PRIVATE_CHANNEL).send(embed=embedparam2)
+            secret_sheet.delete_rows(3)
+            secret_sheet.insert_row([vals[0]], 3)
 
     elif message.content.startswith(
             '--send_msg') and message.author.id == DEVELOPER_ID and message.channel.id == DEVELOPER_SEND_CHANNEL:
